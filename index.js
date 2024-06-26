556712d1069df01ebf033911a7e4dd1f33209040
Marcel Roth <Fiasmo.mr@gmail.com>
2024-06-26 02:15:25 +0200

    Update index.js
    Middle Solution

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,19 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const original_url = []
+const short_url = []
+
+app.post('/api/shorturl', function(req, res){
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+
+  if(foundIndex < 0){
+    
+  }
+  res.json(req.body.url)
 });
 
 app.listen(port, function() {
